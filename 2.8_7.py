406ea15bafcec73b85ee5c0c87e77b9a2a6d0400
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-07-11 17:21:59 +0200

    Create 2.8_7.py

---
+++ 2.8_7.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+
+class ClassDataset(data.Dataset):
+    def __init__(self):
+        self.data_x = 
+        self.data_y = 
+  
