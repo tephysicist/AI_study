8419237bf7da66a09cdc02ef608ec8b857722b95
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-07-28 06:14:49 +0200

    Update 2.8_6.py
    corrected self.coord_y and indents

--- 2.8_6.py
+++ 2.8_6.py
@@ -2,18 +2,18 @@ import torch
 import torch.nn as nn
 import torch.utils.data as data
 
-
+# здесь продолжайте программу
 class FuncDataset(data.Dataset): # имя класса MyDataset может быть любым
-	def __init__(self): # инициализация переменных объекта класса
-    	self.coord_x = torch.arange(-5, 5, 0.1)
-        self.coord_y = 2*torch.exp(-x/2) + 0.2*torch.sin(x/10) - 5
+    def __init__(self): # инициализация переменных объекта класса
+        self.coord_x = torch.arange(-5, 5, 0.1)
+        self.coord_y = 2*torch.exp(-self.coord_x/2) + 0.2*torch.sin(self.coord_x/10) - 5
         self.length = len(self.coord_x)
  
-	def __getitem__(self, item): # возвращение образа выборки по индексу item
-		return self.coord_x[item], self.coord_y[item]
+    def __getitem__(self, item): # возвращение образа выборки по индексу item
+        return self.coord_x[item], self.coord_y[item]
  
-	def __len__(self): # возвращение размера выборки
-    	return self.length
+    def __len__(self): # возвращение размера выборки
+        return self.length
 
 
 d_train = FuncDataset()
