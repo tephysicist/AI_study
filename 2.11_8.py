712020a97cd319e8e6c4d18fdb133b734c8fe0ca
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-08-11 17:00:08 +0200

    Update 2.11_8.py

--- 2.11_8.py
+++ 2.11_8.py
@@ -6,17 +6,18 @@ import torchvision.transforms as tfs
 
 # здесь объявляйте класс ToDtypeV1
 class ToDtypeV1(nn.Module):
-    def __init__(self, dtype=torch.float32, scale=False):
+    def __init__(self, dtype, scale=False):
+        super().__init__()
         self.dtype = dtype
         self.scale = scale
         
-    def forward(self, item):
-        #item = item.to(self.dtype)
-        norm = tfs.Normalize(mean=[0.5], std=[0.5])
-        if (self.dtype == torch.float16 or self.dtype == torch.float32 or self.dtype == torch.float64) and self.scale:
-            return item.to(self.dtype)
-        else:
-            return norm(item) 
+    def forward(self, x):
+        x = x.to(self.dtype)
+        if self.scale and self.dtype in (torch.float16, torch.float32, torch.float64):
+            x_min = x.min()
+            x_max = x.max()
+            x = (x - x_min)/x_max
+        return x 
 
 
 
@@ -24,9 +25,10 @@ H, W = 128, 128
 img_orig = torch.randint(0, 256, size=(3, H, W), dtype=torch.uint8) # тензор в программе не менять
 
 
-img_mean =[torch.mean(img_orig[i], dim=0) for i in range(3)] # средние для каждого цветового канала (первая ось)
-img_std = [torch.std(img_orig[i], dim=0) for i in range(3)] # стандартное отклонение для каждого цветового канала (первая ось)
+img_mean =torch.mean(img_orig.float(), [1, 2]) # средние для каждого цветового канала (первая ось)
+img_std = torch.std(img_orig.float().flattern(1, 2), dim=1) for i in range(3)] # стандартное отклонение для каждого цветового канала (первая ось)
 
 
 # здесь продолжайте программу
-tr = tfs.Compose([tfs.ToDtypeV1(dtype=torch.float32, scale=False), tfs.Resize(128), tfs.ToTensor()])
+tr = tfs.Compose([tfs.ToDtypeV1(dtype=torch.float32, scale=False), tfs.Normalize(mean=[img_mean], std=[img_std])])
+img = tr(img_orig)
