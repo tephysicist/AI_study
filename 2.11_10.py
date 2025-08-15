4cf5c45a725c98685805e19b70a6f6ac1874cc8f
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-08-15 21:12:38 +0200

    Aktualisieren von 2.11_10.py

--- 2.11_10.py
+++ 2.11_10.py
@@ -1,4 +1,6 @@
 import torch
 
 t_rnd = torch.randint(-3, 5, (100, ), dtype=torch.float32) # значения этого тензора в программе не менять
-t_mean = torch.mean(t_rnd)
+t_mean = torch.mean(t_rnd).item()
+t_max = max(t_rnd).item()
+min(t_rnd).item()
