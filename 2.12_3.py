2f237f051161fbb560dcbeb5b842df2be3a40314
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-08-19 15:29:18 +0200

    Update 2.12_3.py
    Corrected dict

--- 2.12_3.py
+++ 2.12_3.py
@@ -8,6 +8,6 @@ layer2 = torch.rand(32, 10)
 bias2 = torch.rand(10)
 
 # здесь продолжайте программу
-data_w = OrderedDict({'layer1': layer1, 'bias1': bias1, 'layer2': layer2, 'bias2', bias2})
+data_w = OrderedDict({'layer1': layer1, 'bias1': bias1, 'layer2': layer2, 'bias2': bias2})
 torch.save(data_w, 'data_w.tar')
 data_w2 = torch.load('data_w.tar')
