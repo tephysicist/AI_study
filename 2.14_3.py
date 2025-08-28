18ce678a01f93d219452a8259f5ac7d4040a7677
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-08-28 17:00:06 +0200

    Create 2.14_3.py

---
+++ 2.14_3.py
@@ -0,0 +1,84 @@
+import torch
+import torch.utils.data as data
+import torch.nn as nn
+import torch.optim as optim
+
+
+class FuncModel(nn.Module):
+    def __init__(self, in_features):
+        super().__init__()
+        self.layer = nn.Linear(in_features=in_features, out_features=1) # полносвязный слой: число входов in_features, число нейронов 1
+
+
+    def forward(self, x):
+        return self.layer(x)
+
+
+
+
+torch.manual_seed(1)
+
+
+epochs = 20 # число эпох обучения
+batch_size = 8 # размер батча
+N = 6 # порядок модели (N-1)
+
+
+# формирование обучающей выборки (значений функции)
+data_x = torch.arange(-4, 4, 0.01)
+data_y = 0.1 * data_x + 0.1 * data_x ** 2 - 0.5 * torch.sin(2 * data_x) + torch.cos(4 * data_x)
+data_x.unsqueeze_(-1)
+X = torch.cat([data_x ** _n for _n in range(N)], dim=1)
+ds = data.TensorDataset(X, data_y)
+
+
+d_train, d_val = data.random_split(ds, [0.8, 0.2]) #разделите выборку ds на две части в пропорции: 80% и 20%
+train_data = data.DataLoader(d_train, batch_size=batch_size, shuffle=True)
+train_data_val = data.DataLoader(d_val, batch_size=len(d_val), shuffle=False)
+
+
+model = FuncModel(N)
+optimizer = optim.RMSprop(params=model.parameters(), lr=0.01) # создайте оптимизатор RMSprop для модели model с шагом обучения 0.01 и weight_decay=10
+loss_func = nn.MSELoss() # сформируйте функцию потерь с помощью класса MSELoss
+
+
+loss_lst_val = []  # список значений потерь при валидации
+loss_lst = []  # список значений потерь при обучении
+
+
+for _e in range(epochs):
+    # переведите модель в режим обучения
+    loss_mean = 0
+    lm_count = 0
+
+
+    for x_train, y_train in train_data:
+        predict = # вычислите прогноз модели для x_train
+        loss = # вычислите потери для predict и y_train
+
+
+        # выполните один шаг обучения (градиентного спуска)
+
+
+        lm_count += 1
+        loss_mean = 1 / lm_count * loss.item() + (1 - 1 / lm_count) * loss_mean
+
+
+    # валидация модели
+    # переведите модель в режим эксплуатации
+    x_val, y_val = next(iter(train_data_val))
+
+
+    with torch.no_grad():
+        p = вычислите прогноз модели для x_val
+        loss = вычислите потери для p и y_val
+        Q_val = loss.item()
+
+
+    loss_lst.append(loss_mean)
+    loss_lst_val.append(Q_val)
+
+
+# переведите модель в режим эксплуатации
+p = # вычислите прогноз модели по всем данным выборки X
+Q = # вычислите потери с помощью loss_func для p и data_y; значение Q сохраните в виде вещественного числа
