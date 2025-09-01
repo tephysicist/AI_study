390d34d1fdd1e135f9d138aa13643cb21b623ff2
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-09-01 17:26:42 +0200

    Create 2.14_6.py

---
+++ 2.14_6.py
@@ -0,0 +1,20 @@
+import torch
+import torch.utils.data as data
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class Funcnn(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features=13, out_features=32)
+        self.layer2 = nn.Linear(in_features=32, out_features=16)
+        self.layer3 = nn.Linear(in_features=16, out_features=3)
+    
+    def forward(self, x):
+        x = F.relu()
+        return x
+    
+
+model.eval()
+x = torch.rand(13)
