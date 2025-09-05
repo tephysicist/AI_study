aa9cda2d94bda54cfb7d896b14c5b53a39515dd2
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-09-05 13:34:53 +0200

    Update 2.14_6.py

--- 2.14_6.py
+++ 2.14_6.py
@@ -10,15 +10,17 @@ class Funcnn(nn.Module):
         self.layer1 = nn.Linear(in_features=13, out_features=32)
         self.layer2 = nn.Linear(in_features=32, out_features=16)
         self.layer3 = nn.Linear(in_features=16, out_features=3)
+        self.dropout = nn.Dropout(0.4)
     
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
+        x = self.dropout(x)
         x = self.layer3(x)
         return x
     
-
+torch.manual_seed(1)
 model = Funcnn()
 model.eval()
 x = torch.rand(13)
-predict = model(x)
+predict = model(x) # x.unsqueeze(0)
