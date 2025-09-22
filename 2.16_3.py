4dc228ae7ebe4fadfbb377cbd246290d354479c7
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-09-22 16:52:40 +0200

    Update 2.16_3.py

--- 2.16_3.py
+++ 2.16_3.py
@@ -5,10 +5,13 @@ batch_size=16
 x = torch.rand(batch_size, 12) # тензор x в программе не менять
 
 # здесь продолжайте программу
-# здесь продолжайте программу
-model = nn.Sequential(
-	nn.Linear(12, 24, bias=True),
-	nn.Tanh(),
-    nn.Linear(24, 10, bias=True),
-    nn.Linear(10, 1, bias=True)
-)
+# I need more time to evaluate.
+model = nn.Sequential()
+model.add_module('layer_1', nn.Linear(12, 24, bias=True))
+model.add_module('act1', nn.Tanh())
+model.add_module('layer_2', nn.Linear(24, 10, bias=True))
+model.add_module('act2', nn.Tanh())
+model.add_module('out', nn.Linear(10, 1, bias=True))
+
+model.eval()
+predict = model(x)
