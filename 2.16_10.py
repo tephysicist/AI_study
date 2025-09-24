d7bab5474e29133e5d950393af71695d470d129d
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-09-24 17:25:44 +0200

    Create 2.16_10.py

---
+++ 2.16_10.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class NN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+batch_size = 100
+x = torch.rand(batch_size, 64) # тензор x в программе не менять
+
+
+# здесь продолжайте программу
