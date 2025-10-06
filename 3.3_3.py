daa1564d436a79a5c8bb79c2c281e371a6c26821
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-10-06 17:27:36 +0200

    Create 3.3_3.py

---
+++ 3.3_3.py
@@ -0,0 +1,20 @@
+import torch
+import torch.utils.data as data
+import torch.nn as nn
+import torch.optim as optim
+
+ds = data.TensorDataset(_global_var_data_x, _global_var_target)  # _global_var_data_x - тензор с изображениями цифр размерностью (total, 1, 8, 8) и типом float32; _global_var_data_y - тензор с целочисленными метками классов размерностью (total, ) и типом long (int64).
+
+model = nn.Sequential(
+    nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=True),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0, dilation=1, return_indices=False, ceil_mode=False),
+    nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=True),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0, dilation=1, return_indices=False, ceil_mode=False),
+    nn.Flatten(),
+    nn.Linear(64, 10, bias=True)
+)
+
+model.train()
+
