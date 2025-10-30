eceffc9ea518d458ff1a9d2b09f09c4dcf50a386
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-10-30 14:23:19 +0100

    Update 3.2_7.py

--- 3.2_7.py
+++ 3.2_7.py
@@ -4,6 +4,6 @@ import torch.nn as nn
 H, W = 32, 25
 x = torch.randint(0, 255, (H, W), dtype=torch.float32) # тензор x в программе не менять
 
-layer = torch.nn.MaxPool2d(kernel_size=(3, 2), stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False) # lr = nn.MaxPool2d((3, 2))
+layer = torch.nn.MaxPool2d(kernel_size=(3, 2), stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
 
-t_out = layer(x) # lr(x.view(1, 1, H, W))
+t_out = lr(x.view(1, 1, H, W))
