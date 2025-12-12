29393b2ea607da3b371feb065485a4e31c99430b
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-12-12 15:09:53 +0100

    Create 3.10_3.py

---
+++ 3.10_3.py
@@ -0,0 +1,16 @@
+import torch
+from torchvision import models
+import torchvision.transforms.functional as TF
+
+x = torch.randint(0, 255, (3, 128, 128), dtype=torch.float32)  
+img_pil = TF.to_pil_image(x)
+
+weights = models.ResNet34_Weights.DEFAULT
+transforms = weights.transforms()
+
+model = models.resnet34()
+model.requires_grad_(False)
+model.eval()
+
+img = transforms(img_pil).unsqueeze()
+results = model(img.unsqueeze(0))
