9a9e2abc6b809495175a0f277a0b6490b345fc95
tephysicist <82123136+tephysicist@users.noreply.github.com>
2025-12-27 12:44:56 +0100

    Update 3.10_5.py

--- 3.10_5.py
+++ 3.10_5.py
@@ -10,10 +10,10 @@ img_pil = TF.to_pil_image(x)
 
 
 # здесь продолжайте программу
-weights = models.models.ResNet50_Weights.DEFAULT
+weights = models.ResNet50_Weights.DEFAULT
 transforms = weights.transforms()
 
-model = model.resnet50()
+model = models.resnet50()
 model.requires_grad_(True)
 model.fc = nn.Sequential(
     nn.Linear(512*4, 100, bias=True),
